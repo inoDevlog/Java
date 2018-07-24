7faf934405e9f9a40688fa6b4170b35268effb43
inoDevlog <ino0319@gmail.com>
2018-07-25 01:12:45 +0900

    no message

--- javajungsuk3/src/ch2/VarEx2.java
+++ javajungsuk3/src/ch2/VarEx2.java
@@ -6,13 +6,13 @@ public class VarEx2 {
 		int x = 10, y = 20;
 		int tmp = 0;
 
-		System.out.println("x:" + x + "y:" + y);
+		System.out.println(" x: " + x + " y: " + y);
 
 		tmp = x;
 		x = y;
 		y = tmp;
 
-		System.out.println("x:" + x + "y:" + y);
+		System.out.println(" x: " + x + " y: " + y);
 
 	}
 
