71a9c87dba2458c91427c2fdcfe0b746f69b797b
inoDevlog <ino0319@gmail.com>
2018-09-20 22:53:39 +0900

    no message

--- javajungsuk3/src/ch3/OperatorEx5.java
+++ javajungsuk3/src/ch3/OperatorEx5.java
@@ -3,7 +3,10 @@ package ch3;
 public class OperatorEx5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 4;
+
+		System.out.println(a % b);
 
 	}
 
