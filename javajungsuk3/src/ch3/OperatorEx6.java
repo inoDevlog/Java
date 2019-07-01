9d11ab6ac389a151e3013697f4bb3a06fcd71551
inoDevlog <ino0319@gmail.com>
2019-07-01 23:44:48 +0900

    변경

--- javajungsuk3/src/ch3/OperatorEx6.java
+++ javajungsuk3/src/ch3/OperatorEx6.java
@@ -3,10 +3,11 @@ package ch3;
 public class OperatorEx6 {
 	public static void main(String[] args) {
 		byte a = 10;
-		byte b = 20;
+		byte b = a++;
 		byte c = (byte) (a + b);
 
 		System.out.println(c);
+		System.out.println(a);
 	}
 
 }
