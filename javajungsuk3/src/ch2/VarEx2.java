dc78675120b730452eb2864b031e0d543cf94ac7
inoDevlog <ino0319@gmail.com>
2019-07-09 01:29:25 +0900

    대입

--- javajungsuk3/src/ch2/VarEx2.java
+++ javajungsuk3/src/ch2/VarEx2.java
@@ -3,14 +3,13 @@ package ch2;
 public class VarEx2 {
 
 	public static void main(String[] args) {
-		int x = 10, y = 20;
-		int tmp;
+		int x = 10;
+		int y = 20;
 		System.out.println(" x: " + x + " y: " + y);
-
-		tmp = x;
+		
+		int tmp = x;
 		x = y;
 		y = tmp;
-
 		System.out.println(" x: " + x + " y: " + y);
 	}
 }
