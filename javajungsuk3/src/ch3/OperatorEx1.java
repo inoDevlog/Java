2b0a19cca3e68ee02c1555f532e377f1ff44c276
inoDevlog <ino0319@gmail.com>
2019-07-16 00:12:31 +0900

    ++

--- javajungsuk3/src/ch3/OperatorEx1.java
+++ javajungsuk3/src/ch3/OperatorEx1.java
@@ -3,7 +3,6 @@ package ch3;
 public class OperatorEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int i = 5;
 		i++;
 		System.out.println(i);
--- javajungsuk3/src/ch3/OperatorEx2.java
+++ javajungsuk3/src/ch3/OperatorEx2.java
@@ -3,15 +3,14 @@ package ch3;
 public class OperatorEx2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int i = 5, j = 0;
-		j = i++;
+		j = i++; // 대입 후에 ++
 		System.out.println("j=i++; 실행 후, i=" + i + ", j=" + j);
 
 		i = 5;
 		j = 0;
-		j = ++i;
+		j = ++i; // ++ 후에 대입
 		System.out.println("j=i++; 실행 후, i=" + i + ", j=" + j);
 	}
 }
