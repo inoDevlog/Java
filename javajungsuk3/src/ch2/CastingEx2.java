19407c3f6dd9bae6fbe2d5fb978ca205beea56f7
inoDevlog <ino0319@gmail.com>
2019-07-15 23:20:35 +0900

    test

--- javajungsuk3/src/ch2/CastingEx2.java
+++ javajungsuk3/src/ch2/CastingEx2.java
@@ -4,7 +4,7 @@ public class CastingEx2 {
 
 	public static void main(String[] args) {
 		int i = 10;
-		byte b = (byte) i;
+		byte b = (byte)i;
 		System.out.printf("[int -> byte] i=%d -> b=%d%n", i, b);
 
 		i = 300;
