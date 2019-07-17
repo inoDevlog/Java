649f2adba7c8f0b5ee655c57b5473e4f806d958b
inoDevlog <ino0319@gmail.com>
2019-07-17 23:37:56 +0900

    java

--- javajungsuk3/src/ch3/OperatorEx5.java
+++ javajungsuk3/src/ch3/OperatorEx5.java
@@ -1,7 +1,6 @@
 package ch3;
 
 public class OperatorEx5 {
-
 	public static void main(String[] args) {
 		int a = 10;
 		int b = 4;
--- javajungsuk3/src/ch3/OperatorEx6.java
+++ javajungsuk3/src/ch3/OperatorEx6.java
@@ -3,11 +3,12 @@ package ch3;
 public class OperatorEx6 {
 	public static void main(String[] args) {
 		byte a = 10;
-		byte b = a++;
+		byte b = ++a;
 		byte c = (byte) (a + b);
 
 		System.out.println(c);
 		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
