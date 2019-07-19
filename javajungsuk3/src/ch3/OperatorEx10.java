82e82763e2a1c11467ad1986cf4ecfa9b1c1ee90
inoDevlog <ino0319@gmail.com>
2019-07-19 23:58:07 +0900

    overflow

---
+++ javajungsuk3/src/ch3/OperatorEx10.java
@@ -0,0 +1,15 @@
+package ch3;
+
+public class OperatorEx10 {
+	public static void main(String[] args) {
+		int a = 1000000;
+		
+		int result1 = a * a / a;
+		int result2 = a / a * a;
+		
+		System.out.println(result1);
+		System.out.println(result2);
+		
+	}
+
+}
