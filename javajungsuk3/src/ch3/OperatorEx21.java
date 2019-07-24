36beaf0bc16a7b583e7ecd1f49c7265a3a052678
inoDevlog <ino0319@gmail.com>
2019-07-24 23:41:24 +0900

    java

---
+++ javajungsuk3/src/ch3/OperatorEx21.java
@@ -0,0 +1,11 @@
+package ch3;
+
+public class OperatorEx21 {
+	public static void main(String[] args) {
+		System.out.println(10==10.0f); // true
+		System.out.println('0'==0); //false
+		System.out.println('A'==65); //true
+		System.out.println('A' > 'B'); //false
+		System.out.println('A'+1 != 'B'); //false
+	}
+}
