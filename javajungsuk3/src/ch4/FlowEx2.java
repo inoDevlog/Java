931f1d21ac98c7c290eeff83cf5ee900a3c281e3
inoDevlog <ino0319@gmail.com>
2019-07-28 23:56:04 +0900

    FlowEx2

---
+++ javajungsuk3/src/ch4/FlowEx2.java
@@ -0,0 +1,25 @@
+package ch4;
+
+import java.util.Scanner;
+
+public class FlowEx2 {
+	public static void main(String[] args) {
+		int input;
+
+		System.out.print("insert Number!------->");
+
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine();
+		input = Integer.parseInt(tmp);
+
+		if (input == 0) {
+			System.out.println("0");
+		}
+
+		if (input != 0) {
+			System.out.println("not 0");
+			System.out.printf("%d", input);
+		}
+	}
+
+}
