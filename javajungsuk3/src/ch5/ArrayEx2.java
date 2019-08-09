d0fa3824e08077e8b70e702de5be710822c4d75c
inoDevlog <ino0319@gmail.com>
2019-08-10 01:34:22 +0900

    Array

--- javajungsuk3/src/ch5/ArrayEx2.java
+++ javajungsuk3/src/ch5/ArrayEx2.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class ArrayEx2 {
 	public static void main(String[] args) {
+		
 		int[] iArr1 = new int[10];
 		int[] iArr2 = new int[10];
 		// int [] iArr3 = new int[] {100,95,80,70,60};
@@ -13,12 +14,15 @@ public class ArrayEx2 {
 		for (int i = 0; i < iArr1.length; i++) {
 			iArr1[i] = i + 1;
 		}
+		
 		for (int i = 0; i < iArr2.length; i++) {
 			iArr2[i] = (int) (Math.random() * 10) + 1;
 		}
+		
 		for (int i = 0; i < iArr1.length; i++) {
 			System.out.print(iArr1[1] + ",");
 		}
+		
 		System.out.println();
 		System.out.println(Arrays.toString(iArr2));
 		System.out.println(Arrays.toString(iArr3));
