442b980d3d9f38bf11f48de543f1b30dd3750756
inoDevlog <ino0319@gmail.com>
2019-08-15 01:34:07 +0900

    array

---
+++ javajungsuk3/src/ch5/ArrayEx6.java
@@ -0,0 +1,22 @@
+package ch5;
+
+public class ArrayEx6 {
+	public static void main(String[] args) {
+		int[] score = { 80, 90, 100, 56, 70, 80, 76 };
+
+		int max = score[0];
+		int min = score[0];
+
+		for (int i = 1; i < score.length; i++) {
+			if (score[i] > max) {
+				max = score[i];
+			} else if (score[i] < min) {
+				min = score[i];
+			}
+		}
+		System.out.println("최대값 :" + max);
+		System.out.println("최소값 :" + min);
+
+	}
+
+}
