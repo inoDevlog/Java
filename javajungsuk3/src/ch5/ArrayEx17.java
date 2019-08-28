cd817d5e999dae7331a0262b35404b40eeb8754a
inoDevlog <ino0319@gmail.com>
2019-08-29 00:31:26 +0900

    args3

--- javajungsuk3/src/ch5/ArrayEx17.java
+++ javajungsuk3/src/ch5/ArrayEx17.java
@@ -11,6 +11,26 @@ public class ArrayEx17 {
 		char op = args[1].charAt(0);
 		int num2 = Integer.parseInt(args[2]);
 		int result = 0;
-	}
 
+		switch (op) {
+		case '+':
+			result = num1 + num2;
+			break;
+
+		case '-':
+			result = num1 - num2;
+			break;
+
+		case '*':
+			result = num1 * num2;
+			break;
+
+		case '/':
+			result = num1 / num2;
+			break;
+		default:
+			System.out.println("지원되지 않는 연산입니다.");
+		}
+		System.out.println("결과:" + result);
+	}
 }
