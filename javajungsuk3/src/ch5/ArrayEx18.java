4d44ba63ffbecd0adf26bb4fd143cd2e6de23619
inoDevlog <ino0319@gmail.com>
2019-09-01 23:26:20 +0900

    array

--- javajungsuk3/src/ch5/ArrayEx18.java
+++ javajungsuk3/src/ch5/ArrayEx18.java
@@ -2,7 +2,12 @@ package ch5;
 
 public class ArrayEx18 {
 	public static void main(String[] args) {
-		int[][] score = { { 100, 100, 100 }, { 20, 20, 20 }, { 30, 30, 30 }, { 40, 40, 40 }, };
+		int[][] score = { 
+							{ 100, 100, 100 }, 
+							{ 20, 20, 20 }, 
+							{ 30, 30, 30 }, 
+							{ 40, 40, 40 },
+						};
 		int sum = 0;
 
 		for (int i = 0; i < score.length; i++) {
