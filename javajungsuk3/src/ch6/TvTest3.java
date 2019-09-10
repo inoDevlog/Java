f72e1d197590a2ed2c6983bc4ac9cef44f6e496a
inoDevlog <ino0319@gmail.com>
2019-09-11 01:21:34 +0900

    TvTest3

--- javajungsuk3/src/ch6/TvTest3.java
+++ javajungsuk3/src/ch6/TvTest3.java
@@ -18,16 +18,19 @@ class Tv {
 	}
 }
 
-class TvTest2 {
+class TvTest3 {
 	public static void main(String[] args) {
 		Tv t1 = new Tv();
 		Tv t2 = new Tv();
+
 		System.out.println(t1.channel);
 		System.out.println(t2.channel);
 
+		t2 = t1;
 		t1.channel = 7;
-		System.out.println("t1의 channel 값을 7로 변경하였습니다.");
+		System.out.println("7로 변경");
 		System.out.println(t1.channel);
 		System.out.println(t2.channel);
 	}
+
 }
