7c69c7b977b61ce5b9900ac2820f60f1c34e31bc
inoDevlog <ino0319@gmail.com>
2019-09-11 01:41:24 +0900

    class

--- javajungsuk3/src/ch6/TvTest2.java
+++ javajungsuk3/src/ch6/TvTest2.java
@@ -1,23 +1,5 @@
 package ch6;
 
-class Tv {
-	String color;
-	boolean power;
-	int channel;
-
-	void power() {
-		power = !power;
-	}
-
-	void channelUp() {
-		++channel;
-	}
-
-	void channelDown() {
-		--channel;
-	}
-}
-
 class TvTest2 {
 	public static void main(String[] args) {
 		Tv t1 = new Tv();
--- javajungsuk3/src/ch6/TvTest3.java
+++ javajungsuk3/src/ch6/TvTest3.java
@@ -1,24 +1,6 @@
 package ch6;
 
-class Tv {
-	String color;
-	boolean power;
-	int channel;
-
-	void power() {
-		power = !power;
-	}
-
-	void channelUp() {
-		++channel;
-	}
-
-	void channelDown() {
-		--channel;
-	}
-}
-
-class TvTest3 {
+class TvTest3  {
 	public static void main(String[] args) {
 		Tv t1 = new Tv();
 		Tv t2 = new Tv();
