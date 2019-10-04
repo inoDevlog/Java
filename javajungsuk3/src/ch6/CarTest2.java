643313858a5b4478f3c1b14a98f045554d778f61
최인호 <ino0319@gmail.com>
2019-10-04 17:41:22 +0900

    class 명 변경

--- javajungsuk3/src/ch6/CarTest2.java
+++ javajungsuk3/src/ch6/CarTest2.java
@@ -1,19 +1,19 @@
 package ch6;
 
-class Car {
+class Car2 {
 	String color;
 	String gearType;
 	int door;
 
-	Car() {
+	Car2() {
 		this("white", "auto", 4);
 	}
 
-	Car(String color) {
+	Car2(String color) {
 		this(color, "auto", 4);
 	}
 
-	Car(String color, String gearType, int door) {
+	Car2(String color, String gearType, int door) {
 		this.color = color;
 		this.gearType = gearType;
 		this.door = door;
@@ -22,8 +22,8 @@ class Car {
 
 public class CarTest2 {
 	public static void main(String[] args) {
-		Car c1 = new Car();
-		Car c2 = new Car("blue");
+		Car2 c1 = new Car2();
+		Car2 c2 = new Car2("blue");
 
 		System.out.println(c1.color + c1.gearType + c1.door);
 		System.out.println(c2.color + c2.gearType + c2.door);
