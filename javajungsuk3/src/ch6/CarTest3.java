cbed732c52080c75d59468d86d0f16560b33ca4a
inoDevlog <ino0319@gmail.com>
2019-10-05 23:58:37 +0900

    test

--- javajungsuk3/src/ch6/CarTest3.java
+++ javajungsuk3/src/ch6/CarTest3.java
@@ -8,13 +8,11 @@ class Car3 {
 	Car3() {
 		this("white", "auto", 4);
 	}
-
 	Car3(Car3 c) {
 		color = c.color;
 		gearType = c.gearType;
 		door = c.door;
 	}
-
 	Car3(String color, String gearType, int door) {
 		this.color = color;
 		this.gearType = gearType;
