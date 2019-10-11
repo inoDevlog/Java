f884b29a1533e91460cd0504144e1c4eacf66f08
inoDevlog <ino0319@gmail.com>
2019-10-12 00:57:18 +0900

    SuperTest2

--- javajungsuk3/src/ch7/SuperTest2.java
+++ javajungsuk3/src/ch7/SuperTest2.java
@@ -1,17 +1,19 @@
 package ch7;
 
-class SuperTest {
+class SuperTest2 {
 	public static void main(String[] args) {
-		Child c = new Child();
+		Child2 c = new Child2();
 		c.method();
 	}
 }
 
-class Parent {
+class Parent2 {
 	int x = 10;
 }
 
-class Child extends Parent {
+class Child2 extends Parent2 {
+	int x = 20;
+
 	void method() {
 		System.out.println("x=" + x);
 		System.out.println("this.x=" + this.x);
