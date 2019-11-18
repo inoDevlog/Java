18b9ea433b8e59e123fa913ca7f1851976b8823d
inoDevlog <ino0319@gmail.com>
2019-11-18 23:01:39 +0900

    anonymous class

---
+++ javajungsuk3/src/ch7/InnerEx8.java
@@ -0,0 +1,15 @@
+package ch7;
+
+import java.awt.*;
+import java.awt.event.*;
+
+class InnerEx8 {
+	public static void main(String[] args) {
+		Button b = new Button("Start");
+		b.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("ActionEvent occurred!!!");
+			}
+		});
+	}
+}
