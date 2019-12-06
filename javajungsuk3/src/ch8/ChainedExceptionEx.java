c5a57b859d8f64d36018fe58eda24716eecdcb1e
ino <ino@218.38.137.125>
2019-12-06 22:10:23 +0900

    ChainedExceptionEx

--- javajungsuk3/src/ch8/ChainedExceptionEx.java
+++ javajungsuk3/src/ch8/ChainedExceptionEx.java
@@ -1,7 +1,7 @@
 package ch8;
 
 class ChainedExceptionEx {
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		try {
 			install();
 		} catch (InstallException e) {
@@ -9,18 +9,18 @@ class ChainedExceptionEx {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}
+	} // main�� ��
 
 	static void install() throws InstallException {
 		try {
-			startInstall();
-			copyFiles();
+			startInstall(); // 프로그램 설치에 필요한 준비를 한다.
+			copyFiles(); // 파일들을 복사한다.
 		} catch (SpaceException e) {
-			InstallException ie = new InstallException("설치 중 예외발");
+			InstallException ie = new InstallException("설치 중 예외발생");
 			ie.initCause(e);
 			throw ie;
 		} catch (MemoryException me) {
-			InstallException ie = new InstallException("설치 중 예외발");
+			InstallException ie = new InstallException("설치 중 예외발생");
 			ie.initCause(me);
 			throw ie;
 		} finally {
@@ -35,43 +35,41 @@ class ChainedExceptionEx {
 
 		if (!enoughMemory()) {
 			throw new MemoryException("메모리가 부족합니다.");
-			throw new RuntimeException(new MemoryException("메모리가 부족합니다."));
 		}
-	}
+	} // startInstall
 
 	static void copyFiles() {
-		// 파일들을 복사하는 코드를 적는다.
+
 	}
 
 	static void deleteTempFiles() {
-		// 임시파일들을 삭제하는 코드를 적는다.
 	}
-	
+
 	static boolean enoughSpace() {
-		// 설치하는데 필요한 공간이 있는지 확인하는 코드를 적는다.
+
 		return false;
 	}
 
 	static boolean enoughMemory() {
-		// 설치하는데 필요한 메모리 공간이 있는지 확인하는 코드를 적는다.
+		// 설치하는데 필요한 메모리공간이 있는지 확인하는 코드를 적는다.
 		return true;
 	}
+} // ExceptionTest
 
-	class InstallException extends Exception {
-		InstallException(String msg) {
-			super(msg);
-		}
+class InstallException extends Exception {
+	InstallException(String msg) {
+		super(msg);
 	}
+}
 
-	class SpaceException extends Exception {
-		SpaceException(String msg) {
-			super(msg);
-		}
+class SpaceException extends Exception {
+	SpaceException(String msg) {
+		super(msg);
 	}
+}
 
-	class MemoryException extends Exception {
-		MemoryException(String msg) {
-			super(msg);
-		}
+class MemoryException extends Exception {
+	MemoryException(String msg) {
+		super(msg);
 	}
 }
